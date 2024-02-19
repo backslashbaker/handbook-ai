01fff8f19a41dc15117aa99bb5040395132bf97b
flocoder404 <folora.duang-arop@madetech.com>
2024-02-19 17:14:55 +0000

    Updated Header test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Header from "../components/Header";
 
-it("renders the Header Component", () => {
+it("renders the Header Component with the title", () => {
   render(<Header />);
   expect(screen.getByText("Chat Bot")).toBeInTheDocument();
 });
