7bdbfef3638731f1407d5e1fe807681687d19e65
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-20 13:58:51 +0000

    added test to check TextInput calls handleChange and onChange event

--- src/tests/TextInput.test.js
+++ src/tests/TextInput.test.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {createEvent, fireEvent, render, screen} from "@testing-library/react";
 import TextInput from '../components/atoms/TextInput';
 
 it("renders the TextInput Component", () => {
     render(<TextInput />);
-    expect(screen.getByTestId('text-input')).toBeInTheDocument();
+    const textBox = screen.getByTestId('text-input');
+    expect(textBox).toBeInTheDocument();
+    expect(textBox.outerHTML).toContain('type="text"')
+});
+
+it("calls the handleChange prop when onChange event is triggered", () => {
+    render(<TextInput />);
+    const onChangeSpy = jest.fn();
+    const textBox = screen.getByTestId('text-input');
+    fireEvent.change(textBox, {
+        target: { value: 'Foo' }
+    });
+    expect(onChangeSpy).toHaveBeenCalled()
 });
