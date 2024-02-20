a8ae71dad057b0351e761bf0d1a17fa99b631b34
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-20 16:05:59 +0000

    test ChatInput sets chatMessage state when SendButton clicked

--- src/tests/ChatInput.test.js
+++ src/tests/ChatInput.test.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ChatInput from '../components/ChatInput';
 
 it("renders the ChatInput Component with correct child components", () => {
@@ -11,4 +12,12 @@ it("renders the ChatInput Component with correct child components", () => {
     expect(chatInput).toBeInTheDocument();
     expect(chatInput).toContainElement(textBox);
     expect(chatInput).toContainElement(button);
+});
+
+it("sets chatMessage when the SendButton is clicked", () => {
+    const setChatMessage = jest.fn();
+    render(<ChatInput setChatMessage={setChatMessage} />);
+    const button = screen.getByTestId('send-button');
+    userEvent.click(button)
+    expect(setChatMessage).toHaveBeenCalled();
 });
