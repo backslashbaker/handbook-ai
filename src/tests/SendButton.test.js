afd9008a3f43c5241b71d5a2609ecd2f52cd181b
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-20 12:16:40 +0000

    tests for send button icon and title

--- src/tests/SendButton.test.js
+++ src/tests/SendButton.test.js
@@ -6,3 +6,15 @@ it("renders the SendButton Component", () => {
     render(<SendButton />);
     expect(screen.getByTestId('send-button')).toBeInTheDocument();
 });
+
+it("renders a fontawesome up arrow icon inside the SendButton Component", () => {
+    render(<SendButton />);
+    const button = screen.getByTestId('send-button');
+    expect(button.outerHTML).toContain('data-icon="arrow-up"')
+});
+
+it("SendButton Component contains a button with the title 'send'", () => {
+    render(<SendButton />);
+    const button = screen.getByTestId('send-button');
+    expect(button.outerHTML).toContain('title="send"')
+});
