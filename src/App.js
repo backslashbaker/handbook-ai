ef0a6f8de259e93a1908c7bab75bfbe7f5c9b154
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-21 11:23:46 +0000

    added ChatInput component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import ChatInput from "./components/ChatInput";
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+        <Header />
+        <ChatInput />
     </div>
   );
 }
