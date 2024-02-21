c566273977effc578f65b4967b9378b504d2beb0
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-21 11:26:12 +0000

    updated App test files

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
   const headerElement = screen.getByText('Chat Bot');
   expect(headerElement).toBeInTheDocument();
