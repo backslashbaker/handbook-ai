ddde886794e5b3988ee526630bd4b0fdb46f10a0
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-22 11:42:02 +0000

    added ChatOutput component to App. states for messages array and user chat message. logic for setting messages state.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Header from './components/Header';
 import ChatInput from "./components/ChatInput";
+import ChatOutput from "./components/ChatOutput";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [chatMessage, setChatMessage] = useState(null);
+    const [messages, setMessages] = useState([]);
 
-  return (
+    useEffect(() => {
+        if (chatMessage) {
+            setMessages((prevMessages) => [...prevMessages, {messageText: chatMessage, fromAi: false}]);
+        }
+    }, [chatMessage])
+
+    return (
     <div className="App">
         <Header />
-        <ChatInput />
+        <ChatOutput messages={messages} />
+        <ChatInput setChatMessage={setChatMessage} />
     </div>
-  );
+    );
 }
 
 export default App;
