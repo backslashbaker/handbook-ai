1989e378af43c7a8e02937e59159bfe997bf0cf5
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-22 11:39:51 +0000

    ChatOutput component and tests

---
+++ src/components/ChatOutput.js
@@ -0,0 +1,21 @@
+import ChatMessage from "./atoms/ChatMessage";
+
+function ChatOutput({messages}) {
+    if (messages) {
+        return (
+            <div className='chat-container' data-testid='chat-output'>
+                {messages.map((message, index) => {
+                    return <ChatMessage key={index} message={message.messageText} messageFromAi={message.fromAi}/>
+                })}
+            </div>
+        )
+    }
+
+    return (
+        <div className='chat-container' data-testid='chat-output'>
+
+        </div>
+    )
+}
+
+export default ChatOutput;
---
+++ src/tests/ChatOutput.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import ChatOutput from '../components/ChatOutput';
+
+it('displays the ChatOutput component', () => {
+    render(<ChatOutput />);
+    const chatOutput = screen.getByTestId('chat-output');
+    expect(chatOutput).toBeInTheDocument();
+});
+
+it('displays messages from user and ai when given an array of objects with the keys "messageText" and "fromAi"', () => {
+    const testMessages = [
+        {messageText: 'Test message from user', fromAi: false},
+        {messageText: 'Test message from ai', fromAi: true}
+    ];
+
+    render(<ChatOutput messages={testMessages} />);
+    const chatOutput = screen.getByTestId('chat-output');
+    const messages = screen.getAllByTestId('chat-message');
+    expect(messages.length).toBe(2);
+    expect(chatOutput.innerHTML).toContain('Test message from user');
+    expect(chatOutput.innerHTML).toContain('Test message from ai');
+});
