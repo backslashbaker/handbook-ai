43397d013b701478b4aa89df7e10d7546a62876d
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-22 10:33:55 +0000

    ChatMessage display test fixed. added test for ChatMessage text

--- src/tests/ChatMessage.test.js
+++ src/tests/ChatMessage.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import ChatMessage from '../components/atoms/ChatMessage';
 
 it('displays the ChatMessage component', () => {
     render(<ChatMessage />);
     const chatMessage = screen.getByTestId('chat-message');
+    expect(chatMessage).toBeInTheDocument();
+});
+
+it('displays the chat message when given a message prop', () => {
+   render(<ChatMessage message={'Test message'} />);
+   const chatMessage = screen.getByTestId('chat-message');
+   expect(chatMessage.innerHTML).toContain('Test message');
 });
