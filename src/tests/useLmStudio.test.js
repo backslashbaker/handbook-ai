3268bd1c036eea957510d2c6416d0d1f248f9f35
bnjn-mt <benjamin.johnson@madetech.com>
2024-02-23 15:47:52 +0000

    removed useLmStudio.test.js. moved css files to an assets folder

--- src/tests/useLmStudio.test.js
+++
@@ -1,7 +0,0 @@
-import {renderHook, act} from "@testing-library/react";
-import useLmStudio from '../hooks/useLmStudio';
-
-it('should exist', () => {
-   const result = useLmStudio();
-   expect(result).toBeDefined();
-});
